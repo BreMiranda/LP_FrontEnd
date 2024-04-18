23eb9836858278b41e9dfe6f7040d1d2df69ff93
Brenda Miranda <brendatw43@gmail.com>
2024-04-18 19:17:00 -0300

    conteúdo aula 9

---
+++ Aula9/main.js
@@ -0,0 +1,10 @@
+let buttons = document.querySelectorAll('a', '.btn');
+let url = window.location.href;
+
+
+
+for(let button of buttons){
+    if (button.href == url){
+        button.classList.add('active');
+    }
+}
