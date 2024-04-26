fb799da61bdd33f3f437a81eeb9f8a9776554a1d
Brenda Miranda <brendatw43@gmail.com>
2024-04-26 19:50:53 -0300

    Trocando de forEach para Array

--- Aula10/Calculadora/main.js
+++ Aula10/Calculadora/main.js
@@ -1,7 +1,8 @@
 let buttons = document.querySelectorAll('.btn');
+let buttonsArray = Array.from(buttons);
 
-buttons.forEach(function (button){
-    button.onclick = function(){
+buttonsArray.forEach(function(button) {
+    button.onclick = function() {
         document.querySelector('#resposta').innerHTML = button.innerHTML;
     }
 });
